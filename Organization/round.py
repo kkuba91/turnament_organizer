ef79956447567daae4a83d6a59658557db8d3269
kkuba91 <53142903+kkuba91@users.noreply.github.com>
2022-12-06 01:07:45 +0100

    Swiss system core initial version (working).

--- Organization/round.py
+++ Organization/round.py
@@ -13,11 +13,22 @@ class Round(ModelRound):
     def add_table(self, player_w, player_b):
         next_nr = len(self.tables) + 1
         table = Table(
-            nr=next_nr,
+            number=next_nr,
             w_player=player_w,
             b_player=player_b
             )
         self.tables[next_nr] = table
+        # Pausing option:
+        if player_w == -1:
+            self.set_result(table_nr=next_nr, result=0.0)
+        elif player_b == -1:
+            self.set_result(table_nr=next_nr, result=1.0)
+        # If Previous table has Parser, swap tables (Parser table must be las one):
+        if next_nr > 1:
+            if self.tables[next_nr-1].w_player == -1 or self.tables[next_nr-1].b_player == -1:
+                buffer_table = self.tables[next_nr-1]
+                self.tables[next_nr-1] = self.tables[next_nr]
+                self.tables[next_nr] = buffer_table
         return next_nr
 
     def set_result(self, table_nr, result):
